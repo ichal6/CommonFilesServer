2fce2ff07788cac2420cf73a6458864fa7642e76
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-08-05 17:59:56 +0200

    Add gitignore

---
+++ src/test/java/com/lechowicz/servercommonfiles/ServerCommonFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lechowicz.servercommonfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerCommonFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
