19bf3e0dd6ec6798fbc5003b4eb87ddba3110707
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-08-10 20:20:05 +0200

    Add custom constructor to Video Record

--- src/main/java/com/lechowicz/servercommonfiles/model/Video.java
+++ src/main/java/com/lechowicz/servercommonfiles/model/Video.java
@@ -3,4 +3,7 @@ package com.lechowicz.servercommonfiles.model;
 import java.util.Date;
 
 public record Video(String id, String name, Date createdDate, String ownerId, long lengthInSeconds, String url) {
+    public Video(String name, Date createdDate, String ownerId, long lengthInSeconds, String url) {
+        this(null, name, createdDate, ownerId, lengthInSeconds, url);
+    }
 }
