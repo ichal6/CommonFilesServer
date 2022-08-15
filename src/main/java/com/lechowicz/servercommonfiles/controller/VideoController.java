e7dab320f216ddd8da6a0e89d82139e4d8a19fc4
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-08-15 21:34:18 +0200

    Add SchemaMapping for owner

--- src/main/java/com/lechowicz/servercommonfiles/controller/VideoController.java
+++ src/main/java/com/lechowicz/servercommonfiles/controller/VideoController.java
@@ -1,11 +1,15 @@
 package com.lechowicz.servercommonfiles.controller;
 
+import com.lechowicz.servercommonfiles.model.Author;
+import com.lechowicz.servercommonfiles.model.Photo;
 import com.lechowicz.servercommonfiles.model.Video;
 import com.lechowicz.servercommonfiles.model.VideoInput;
+import com.lechowicz.servercommonfiles.repository.AuthorRepository;
 import com.lechowicz.servercommonfiles.service.VideoService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -13,9 +17,11 @@ import java.util.List;
 @Controller
 public class VideoController {
     private final VideoService videoService;
+    private final AuthorRepository authorRepository;
 
     public VideoController(VideoService videoService) {
         this.videoService = videoService;
+        this.authorRepository = new AuthorRepository();
     }
 
     @QueryMapping
@@ -35,5 +41,10 @@ public class VideoController {
     @MutationMapping
     Video addVideo(@Argument("input") VideoInput video){
         return this.videoService.addVideo(video);
+    }
+
+    @SchemaMapping
+    public Author owner(Video video) {
+        return authorRepository.getAuthorById(video.ownerId());
     }
 }
