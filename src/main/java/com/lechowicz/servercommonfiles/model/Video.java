9e9d7f1afc67838af6fabf645180ca73ae00aee4
Michael Lechowicz <ichal.lechowicz@gmail.com>
2022-08-15 21:20:40 +0200

    Write test for Offset Time in MongoDB

--- src/main/java/com/lechowicz/servercommonfiles/model/Video.java
+++ src/main/java/com/lechowicz/servercommonfiles/model/Video.java
@@ -1,9 +1,21 @@
 package com.lechowicz.servercommonfiles.model;
 
-import java.util.Date;
+import org.springframework.data.annotation.PersistenceCreator;
 
-public record Video(String id, String name, Date createdDate, String ownerId, long lengthInSeconds, String url) {
-    public Video(String name, Date createdDate, String ownerId, long lengthInSeconds, String url) {
+import java.time.OffsetDateTime;
+
+public record Video(String id, String name, OffsetDateTime createdDate, String ownerId, Long lengthInSeconds, String url) {
+    public Video(String name, OffsetDateTime createdDate, String ownerId, Long lengthInSeconds, String url) {
         this(null, name, createdDate, ownerId, lengthInSeconds, url);
+    }
+
+    @PersistenceCreator
+    public Video(String id, String name, OffsetDateTime createdDate, String ownerId, Long lengthInSeconds, String url) {
+        this.id = id;
+        this.name = name;
+        this.createdDate = createdDate;
+        this.ownerId = ownerId;
+        this.lengthInSeconds = lengthInSeconds;
+        this.url = url;
     }
 }
--- src/test/java/com/lechowicz/servercommonfiles/ServerCommonFilesApplicationTests.java
+++ src/test/java/com/lechowicz/servercommonfiles/ServerCommonFilesApplicationTests.java
@@ -1,13 +1,37 @@
 package com.lechowicz.servercommonfiles;
 
-import org.junit.jupiter.api.Test;
+import com.lechowicz.servercommonfiles.model.Video;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ServerCommonFilesApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void givenSavedVideo_TimeIsRetrievedCorrectly(@Autowired MongoTemplate videoRepository) {
+		String id = "testId";
+		OffsetDateTime date = OffsetDateTime.of(2022, 8, 15, 18, 38, 59, 49, ZoneOffset.UTC);
+
+		videoRepository.save(new Video("video-99", "My Video", date, "author-1", 120L, "C:/"));
+		List<Video> videosList = videoRepository.findAll(Video.class);
+
+		System.out.println(videosList.get(0));
+		assertEquals(1, videosList.size());
+		Video video = videosList.get(0);
+		assertEquals("2022-08-15T18:38:59Z", video.createdDate().toString());
 	}
 
 }
